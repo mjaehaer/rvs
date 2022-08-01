d246cb97fafd4a03bd6f6c4257e624698155080f
Raphael <uxmega@mail.ru>
2022-08-01 12:04:10 +0300

    show method

--- main.cc
+++ main.cc
@@ -6,16 +6,33 @@ typedef std::function<void(const HttpResponsePtr &)> Callback;
 
 void show(const HttpRequestPtr &request, Callback &&callback, std::string &&key) {
     nosql::RedisClientPtr redisClient = app().getRedisClient();
+    std::string command = "get " + key;
     redisClient->execCommandAsync(
 	[callback](const drogon::nosql::RedisResult &r) {
 		std::cout << "OKE" << std::endl;
-		//std::cout << r.asString() << std::endl;
+		std::string redisResponse = r.asString();
+		std::cout << &redisResponse << std::endl;
+		Json::Value response;
+		Json::CharReaderBuilder builder;
+		Json::CharReader *reader = builder.newCharReader();
+		Json::Value json;
+		std::string errors;
+		bool parsingSuccessful = 
+			reader->parse(redisResponse.c_str(),
+				redisResponse.c_str() + redisResponse.size(),
+				&json, &errors);
+		response["response"] = redisResponse;
+		if (parsingSuccessful)
+		{
+			response["response"] = json;
+		}
+		auto resp = HttpResponse::newHttpJsonResponse(response);
+		callback(resp);
 	},	
 	[](const std::exception &err) {
 		LOG_ERROR << "redis not work" << err.what();
 	},
-	"get %s",
-	key
+	command
 );
     std::cout << key << std::endl;
 }
